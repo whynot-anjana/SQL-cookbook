28c052320587dd106932a5fffb72f5be485b799e
Anjana <136672570+whynot-anjana@users.noreply.github.com>
2025-10-17 16:37:48 +0530

    Add files via upload

---
+++ SELECT DISTINCT.sql
@@ -0,0 +1,27 @@
+
+
+CREATE TABLE EmployeeRecords (
+    EmployeeID INT PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Department NVARCHAR(50),
+    Salary DECIMAL(10, 2)
+);
+
+
+INSERT INTO EmployeeRecords (EmployeeID, FirstName, LastName, Department, Salary)
+VALUES
+(1, 'John', 'Miller', 'HR', 60000),
+(2, 'Jane', 'Smith', 'Finance', 75000),
+(3, 'John', 'Miller', 'HR', 60000),
+(4, 'Emily', 'Davis', 'IT', 85000),
+(5, 'Michael', 'Brown', 'Finance', 75000),
+(6, 'Jane', 'Smith', 'Finance', 75000);
+
+
+select distinct FirstName from EmployeeRecords 
+
+select distinct FirstName, LastName from EmployeeRecords
+
+select distinct salary from EmployeeRecords
+
